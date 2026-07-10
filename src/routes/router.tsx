f3162066188b39f3ae6ee65dc4eebaa088b514cc
Serdar Topal <woontegra@gmail.com>
2026-07-10 23:44:27 +0300

    beyaz ekran sorunu devam

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -6,7 +6,6 @@ import { AppRouteErrorBoundary } from '@/components/common/AppRouteErrorBoundary
 import { LazyChunkErrorBoundary } from '@/components/common/LazyChunkErrorBoundary'
 import { ScrollToTop } from '@/components/common/ScrollToTop'
 import { clearChunkReloadAttemptFlag } from '@/lib/chunkLoadError'
-import { applyRecoveryBypassOnBoot, clearRecoverySessionState } from '@/lib/recoveryStorage'
 import { SiteLayout } from '@/layouts/SiteLayout'
 import { PublicRouteSkeleton } from '@/components/public/PublicRouteSkeleton'
 
@@ -226,8 +225,6 @@ function LegacyOrderFailRedirect() {
 
 function RootLayout() {
   useEffect(() => {
-    applyRecoveryBypassOnBoot()
-    clearRecoverySessionState()
     clearChunkReloadAttemptFlag()
   }, [])
 
